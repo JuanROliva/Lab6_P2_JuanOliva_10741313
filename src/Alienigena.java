2a156aef1507772c42bd014fcaea832c1067e021
juanROliva <juan.oliva90@gmail.com>
2022-02-25 13:52:22 -0600

    Commit No. 2

---
+++ src/Alienigena.java
@@ -0,0 +1,47 @@
+
+public class Alienigena {
+
+    private String nombre, raza;
+    private int edad;
+    private boolean amenaza;
+
+    public Alienigena() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isAmenaza() {
+        return amenaza;
+    }
+
+    public void setAmenaza(boolean amenaza) {
+        this.amenaza = amenaza;
+    }
+
+    @Override
+    public String toString() {
+        return "Alienigena" + nombre;
+    }
+}
---
+++ src/Planeta.java
@@ -0,0 +1,71 @@
+
+import java.util.ArrayList;
+
+public class Planeta {
+
+    private String nombrePlaneta;
+    private boolean aguaPresente;
+    private int tamanoPlaneta, temperaturaEnCelcius;
+    private ArrayList<Alienigena> habitantes = new ArrayList<Alienigena>();
+
+    public Planeta() {
+    }
+
+    public Planeta(String nombrePlaneta, boolean aguaPresente,
+            int tamanoPlaneta, int temperaturaEnCelcius) {
+        this.nombrePlaneta = nombrePlaneta;
+        this.aguaPresente = aguaPresente;
+        this.tamanoPlaneta = tamanoPlaneta;
+        this.temperaturaEnCelcius = temperaturaEnCelcius;
+    }
+
+    public String getNombrePlaneta() {
+        return nombrePlaneta;
+    }
+
+    public void setNombrePlaneta(String nombrePlaneta) {
+        this.nombrePlaneta = nombrePlaneta;
+    }
+
+    public boolean isAguaPresente() {
+        return aguaPresente;
+    }
+
+    public void setAguaPresente(boolean aguaPresente) {
+        this.aguaPresente = aguaPresente;
+    }
+
+    public int getTamanoPlaneta() {
+        return tamanoPlaneta;
+    }
+
+    public void setTamanoPlaneta(int tamanoPlaneta) {
+        this.tamanoPlaneta = tamanoPlaneta;
+    }
+
+    public int getTemperaturaEnCelcius() {
+        return temperaturaEnCelcius;
+    }
+
+    public void setTemperaturaEnCelcius(int temperaturaEnCelcius) {
+        this.temperaturaEnCelcius = temperaturaEnCelcius;
+    }
+
+    public ArrayList<Alienigena> getHabitantes() {
+        return habitantes;
+    }
+
+    public void setHabitantes(ArrayList<Alienigena> habitantes) {
+        this.habitantes = habitantes;
+    }
+
+    @Override
+    public String toString() {
+        return nombrePlaneta;
+    }
+
+    public boolean validadNombre(String nombre) {
+        return nombre.toLowerCase().equals(this.nombrePlaneta);
+    }
+
+}
